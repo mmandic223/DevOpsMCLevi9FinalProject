2aed2bff73a911d89bba23b950bcf574be7a5d63
Marko Z Mandic <marko.mandic@mmandic.levi9.com>
2022-08-30 13:30:52 +0200

    sace da radi 100%

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from aws_final_project.Networking.networking import NetworkingtStack
 from aws_final_project.LambdaApiDynamo.lambda_api_dynamo import LambdaApiDynamoStack
 from aws_final_project.EcsCluster.ecs_cluster import EcsClusterStack
 from aws_final_project.Pipeline.pipeline import PipelineStack
+from aws_final_project.GithubActionsConnection.github_actions_connection import GithubConnection
 from dotenv import dotenv_values
 
 
@@ -16,6 +17,8 @@ env_EU = cdk.Environment(account="802288441694", region="eu-west-1")
 
 
 app = cdk.App()
+
+GithubConnection(app, "githubconnection-stack-assume-role-mm")
 networkingstack = NetworkingtStack(app, "networking-stack-mm", env=env_EU)
 LambdaApiDynamoStack(app, "lambda-api-dynamo-stack-mm", networkingstack.vpc_endpoint ,env=env_EU)
 PipelineStack(app, "pipeline-stack", env=env_EU)
---
+++ aws_final_project/GithubActionsConnection/github_actions_connection.py
@@ -0,0 +1,57 @@
+from aws_cdk import (
+    Duration,
+    CfnOutput,
+    aws_iam as iam,
+    App,
+)
+from aws_cdk import Duration, RemovalPolicy, Stack
+from constructs import Construct
+
+class GithubConnection(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        github_org="mmandic223"
+        github_repo="DevOpsMCLevi9FinalProject"
+
+        github_provider = iam.OpenIdConnectProvider(self , "ghprovider",
+            url="https://token.actions.githubusercontent.com",
+            client_ids=["sts.amazonaws.com"]    
+        )
+
+        principle = iam.OpenIdConnectPrincipal(github_provider).with_conditions(
+            conditions={
+                "StringLike": {
+                    'token.actions.githubusercontent.com:sub':
+                        f'repo:{github_org}/{github_repo}:*'
+                }
+            }
+        )
+
+        principle.add_to_principal_policy(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=["sts:AssumeRoleWithWebIdentity"],
+                resources=["*"]
+            )
+        )
+
+        iam.Role(self, "deployment_role",
+            assumed_by=principle,
+            role_name=f"{github_org}-{github_repo}-deploy",
+            max_session_duration=Duration.seconds(3600),
+            inline_policies={
+                "DeploymentPolicy": iam.PolicyDocument(
+                    statements=[
+                        iam.PolicyStatement(
+                            actions=['sts:AssumeRole'],
+                            resources=[f'arn:aws:iam::982195495700:role/cdk-*'],
+                            effect=iam.Effect.ALLOW
+                        )
+                    ]
+                )
+            }
+        )
+
+        CfnOutput(self, "DeploymentRoleArn", value=f"arn:aws:iam::982195495700:role/{github_org}-{github_repo}-deploy")
