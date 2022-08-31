ad185667318e479f9662056027491f24862feff1
Marko Z Mandic <marko.mandic@mmandic.levi9.com>
2022-08-31 13:47:57 +0200

    trying to push image v2

--- aws_final_project/GithubActionsConnection/github_actions_connection.py
+++ aws_final_project/GithubActionsConnection/github_actions_connection.py
@@ -61,6 +61,14 @@ class GithubConnection(Stack):
             )
         )
 
+        principle.add_to_principal_policy(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=["ecr:*"],
+                resources=["*"]
+            )
+        )
+
         iam.Role(self, "deployment_role",
             assumed_by=principle,
             role_name=f"{github_org}-{github_repo}-deploy",
@@ -85,6 +93,11 @@ class GithubConnection(Stack):
                         ),
                         iam.PolicyStatement(
                             actions=['sts:*'],
+                            resources=['*'],
+                            effect=iam.Effect.ALLOW
+                        ),
+                        iam.PolicyStatement(
+                            actions=['ecr:*'],
                             resources=['*'],
                             effect=iam.Effect.ALLOW
                         )
